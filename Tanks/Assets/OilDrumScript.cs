79c0190b38d72b9b54ac0f9199ae0cfd79d8b6cb
Hayley Richardson <Hayley Richardson>
2016-10-06 13:43:15 -0700

    tut3

---
+++ Tanks/Assets/OilDrumScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OilDrumScript : MonoBehaviour {
+
+    public ParticleSystem ps;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Tank")
+        {
+            TankHealth targetHealth = other.GetComponent<TankHealth>();
+                //;.GetComponent<TankHealth>();
+            /*if (!targetHealth)
+                continue;*/
+            float damage = 10;
+            targetHealth.TakeDamage(damage);
+            ps.gameObject.transform.position = transform.position;
+            ps.Emit(1000);
+            Destroy(gameObject);
+        }
+    }
+}
